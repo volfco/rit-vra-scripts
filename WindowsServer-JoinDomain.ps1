d04eb0e58b6769ee4f380948fe66588b1684baec
Colum McGaley <cxm7688@rit.edu>
2017-02-20 13:10:09 -0500

    Added DNS Registration

--- WindowsServer-JoinDomain.ps1
+++ WindowsServer-JoinDomain.ps1
@@ -9,6 +9,11 @@ $DomainPassword = "Student1!" | ConvertTo-SecureString -asPlainText -Force
 # =========================================================================================
 
 Write-Output '== Joining to domain ==================='
-Write-Output '= Domain: ' + $Domain
+Write-Output "= Domain: $Domain"
 $credential = New-Object System.Management.Automation.PSCredential($DomainUsername, $DomainPassword)
 Add-Computer -DomainName $Domain -Credential $credential -Force
+
+Write-Output '== Configuring NIC for DNS ============='
+(Get-WmiObject -Class Win32_NetworkAdapter -Filter "NetEnabled=True").GetRelated('Win32_NetworkAdapterConfiguration') | ForEach-Object {
+    $_.SetDynamicDNSRegistration($true,$true)
+}
