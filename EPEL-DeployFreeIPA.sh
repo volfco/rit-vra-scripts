c99ffde714f5bc1d3196b55246d5b43eb951c436
Colum McGaley <cxm7688@rit.edu>
2017-02-20 10:22:43 -0500

    Changed shell to bash, piped yum install output to /dev/null as we don't really need to see that, added absolute paths to commands, and added $DelegatedDomain variable

--- EPEL-DeployFreeIPA.sh
+++ EPEL-DeployFreeIPA.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Copyright 2017 Rochester Institute of Technology
 #   Colum McGaley <cxm7688@rit.edu>
@@ -14,15 +14,17 @@
 #   $ServerName         This server's name. This will be the new hostname
 #   $Password           FreeIPA Administrator and Directory Password
 #   $CAServer           CA Server (dcano1.ca.local or dcano2.ca.local)
+#   $DelegatedDomain
 
 echo "== Mounting Shared Drive ==============="
 mkdir /mnt/shared
-yum -y install cifs-utils
+yum -y install cifs-utils > /dev/null
 mount -t cifs //itsnas01.main.ad.rit.edu/vRAscripts$ /mnt/shared -o ro,username=vrauser,password=Student1!
 
 echo "== Setting Variables ==================="
-USERNAME=$(/usr/bin/python3 /mnt/shared/Common/guesthelpr/src/workitem.py --property virtualmachine.admin.owner --filter username)
-DOMAIN="${DomainTemplate/%VRMOwner%/$USERNAME}"
+USERNAME=$(/usr/bin/python3 /mnt/shared/Components/guesthelpr/src/workitem.py --property virtualmachine.admin.owner --filter username)
+DOMAIN="${DomainTemplate/'%VRMOwner%'/$USERNAME}"
+DOMAIN="${DOMAIN/'%Domain%'/$DelegatedDomain}"
 FQDN="$ServerName.$DOMAIN"
 echo "Username: $USERNAME"
 echo "Domain: $DOMAIN"
@@ -30,11 +32,11 @@ echo "FQDN: $FQDN"
 
 # Install FreeIPA Server
 echo "== Install FreeIPA Server Components ==="
-yum -y install freeipa-server ipa-server-dns
+yum -y install freeipa-server ipa-server-dns > /dev/null
 
 # Src: http://stackoverflow.com/a/33550399
 NET_IF=`netstat -rn | awk '/^0.0.0.0/ {thif=substr($0,74,10); print thif;} /^default.*UG/ {thif=substr($0,65,10); print thif;}'`
-NET_IP=`ifconfig ${NET_IF} | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
+NET_IP=`/usr/sbin/ifconfig ${NET_IF} | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
 
 echo "127.0.0.1     localhost" > /etc/hosts
 echo "$NET_IP     $FQDN " >> /etc/hosts
@@ -42,14 +44,18 @@ hostnamectl set-hostname $(echo "$FQDN")
 
 # Part 1. Install without CA, so we can get the CSR
 echo "== Install FreeIPA Server =============="
-ipa-server-install -r $(echo $DOMAIN) -d $(echo $DOMAIN) -p $(echo $Password) -a $(echo $Password) -U --hostname $(echo "$FQDN") --external-ca --setup-dns --forwarder=172.31.1.1 --forwarder=172.31.1.2 --no-reverse
+/usr/sbin/ipa-server-install -r $(echo $DOMAIN) -d $(echo $DOMAIN) -p $(echo $Password) -a $(echo $Password) -U --hostname $(echo "$FQDN") --external-ca --setup-dns --forwarder=172.31.1.1 --forwarder=172.31.1.2 --no-reverse
+
+if [ $? -ne 0]; then
+  exit 1
+fi
 
 echo "== Signing CA Cert ====================="
-python3 /mnt/shared/Components/certsrv/src/certsrv.py --hostname $(echo $CAServer) --csr /root/ipa.csr --crt /root/ipa.crt --include-chain --no-ssl --verbose
+/usr/bin/python3 /mnt/shared/Components/certsrv/src/certsrv.py --hostname $(echo $CAServer) --csr /root/ipa.csr --crt /root/ipa.crt --include-chain --no-ssl --verbose
 
 # TODO Add synthetic entropy to drop the run time of the below command
 echo "== Finishing up ========================"
-ipa-server-install -r $(echo $DOMAIN) -p $(echo $Password) -a $(echo $Password) -U --external-cert-file=/root/ipa.crt
+/usr/sbin/ipa-server-install -r $(echo $DOMAIN) -p $(echo $Password) -a $(echo $Password) -U --external-cert-file=/root/ipa.crt
 
 echo "== Opening Ports ======================="
 firewall-cmd --add-port=80/tcp --permanent    # HTTP
