bb0419b79949f953139d94e5862d91435d0b47f1
Colum McGaley <cxm7688@rit.edu>
2017-02-26 11:06:24 -0500

    Wrapped Domain in string due to funky vRA variable input

--- WindowsServer-JoinDomain.ps1
+++ WindowsServer-JoinDomain.ps1
@@ -11,7 +11,7 @@ $DomainPassword = "$Password" | ConvertTo-SecureString -asPlainText -Force
 Write-Output '== Joining to domain ==================='
 Write-Output "= Domain: $Domain"
 $credential = New-Object System.Management.Automation.PSCredential($DomainUsername, $DomainPassword)
-Add-Computer -DomainName $Domain -Credential $credential -Force
+Add-Computer -DomainName "$Domain" -Credential $credential -Force -Verbose
 
 Write-Output '== Configuring NIC for DNS ============='
 (Get-WmiObject -Class Win32_NetworkAdapter -Filter "NetEnabled=True").GetRelated('Win32_NetworkAdapterConfiguration') | ForEach-Object {
